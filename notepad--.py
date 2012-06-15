f6b92ece56d8fbb8c1bebada3b3ade825243c93a
Jackson Gatenby <jackson.gatenby@gmail.com>
2012-06-16 00:48:38 +1000

    Application
    Initial version of a very simple text editor.

---
+++ notepad--.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Notepad-- : A very simple text editor."""
+
+import Tkinter as tk
+import tkFileDialog
+
+def create_gui(root):
+    """Set up the GUI and functionality."""
+    root.title("Notepad--")
+    textbox = tk.Text(root)
+    textbox.pack(expand=True, fill=tk.BOTH)
+
+    # Functionality for opening and saving files.
+    def open_file():
+        """Open a file"""
+        f = tkFileDialog.askopenfile()
+        if f:
+            textbox.delete(1.0, tk.END)
+            textbox.insert(tk.END, f.read())
+            f.close()
+
+    def save_file():
+        """Save a file"""
+        f = tkFileDialog.asksaveasfile()
+        if f:
+            f.write(textbox.get(1.0, tk.END)[:-1])
+            f.close()
+
+    # Set up the command buttons
+    frame = tk.Frame(root)
+    frame.pack()
+    tk.Button(frame, text="Open", command=open_file).pack(side=tk.LEFT)
+    tk.Button(frame, text="Save", command=save_file).pack(side=tk.LEFT)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    create_gui(root)
+    root.mainloop()
